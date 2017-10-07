98f8a7b80100ee4bab3205ceb924128ec41e3148
Caleb Uriah Harrison <calebuharrison@gmail.com>
2017-10-07 18:40:07 -0500

    Add configuration methods

--- src/crystimage.cr
+++ src/crystimage.cr
@@ -3,6 +3,24 @@ require "lib_stb_image"
 
 module CrystImage
 
+  @@configuring : Bool = false
+
+  def self.on_load(&block)
+    @@configuring = true
+    yield self
+    @@configuring = false
+  end
+
+  def self.flip_vertically=(b : Bool)
+    raise "Must be configuring to configure!" unless @@configuring
+    LibSTBImage.set_flip_vertically_on_load(b.hash)
+  end
+
+  def self.unpremultiply=(b : Bool)
+    raise "Must be configuring to configure!" unless @@configuring
+    LibSTBImage.set_unpremultiply_on_load(b.hash)
+  end
+
   def self.open(filename : String, channels : Channels = Channels::Default, &block)
     image = Image.new(filename, channels)
     yield image
